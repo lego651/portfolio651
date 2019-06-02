99f4bb0fbff171fdc507ce9aac833be1fa5dfd86
Lego Gao <jasonusdc@gmail.com>
2019-06-01 17:15:35 -0700

    make home header background gif image work.

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './style.css'
+
+const Header = () => {
+  return(
+    <div className="header-container">
+      <div className="header">
+        header...
+      </div>
+    </div>
+  )
+}
+
+export default Header
